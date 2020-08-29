b2f2efee748bdd46307e2ecbf0e9c84d681d753e
Rajvardhan <rajthakur3619@gmail.com>
2020-08-29 12:33:44 +0530

    using raw sql query

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+
 class UserController extends Controller
 {
     public function index(){
+
+        // DB::insert('insert into users (name, email, password) values (?,?,?)', [
+        //     'John','john123@gmail.com','john123',
+        // ]);
+
+        // DB::update('update users set name = ? where id=1', ['Tyrion']);
+
+        DB::delete('delete from users where id=1');
+
+        $users = DB::select('select * from users');
+
+        return $users;
+
         return view('home');
     }
 }
