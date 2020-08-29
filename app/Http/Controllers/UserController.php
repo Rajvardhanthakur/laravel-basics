4deb880dbecad5447e865a054fefccfec1302e7d
Rajvardhan <rajthakur3619@gmail.com>
2020-08-29 13:07:23 +0530

    basics of Eloquent ORM

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,25 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
 
+use App\User;
+
 class UserController extends Controller
 {
     public function index(){
+
+        // $user = new User();
+        // $user->name = 'John';
+        // $user->email = 'joh232@gmail.com';
+        // $user->password = bcrypt('password');
+        // $user->save();
+
+        // var_dump($user);
+        // dd($user); // die and dump
+
+        // $user = User::all();// to get all the records in User table
+        // return $user;
+
+        //User::where('id', 2)->delete();
 
         // DB::insert('insert into users (name, email, password) values (?,?,?)', [
         //     'John','john123@gmail.com','john123',
@@ -16,11 +32,15 @@ class UserController extends Controller
 
         // DB::update('update users set name = ? where id=1', ['Tyrion']);
 
-        DB::delete('delete from users where id=1');
+        // DB::delete('delete from users where id=1');
 
-        $users = DB::select('select * from users');
+        User::where('id', 3)->update(['name' => 'Tyrion']);
 
-        return $users;
+        $user = User::all();
+
+        // $users = DB::select('select * from users');
+
+        return $user;
 
         return view('home');
     }
