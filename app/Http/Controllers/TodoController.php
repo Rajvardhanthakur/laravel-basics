fec798b9c4822960c271740dc9ed4b6e74f3a1bd
Rajvardhan <rajthakur3619@gmail.com>
2020-08-31 22:59:20 +0530

    creating custom made validation

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Validator;
+
+use App\Http\Requests\TodoCreateRequest;
+
 use App\Todo;
 
 class TodoController extends Controller
@@ -21,12 +25,28 @@ class TodoController extends Controller
         return view('todos.edit');
     }
 
-    public function store(Request $request){
+    public function store(TodoCreateRequest $request){
+
+
+        // $rules = [
+        //     'title' => 'required|max:255',
+        // ];
+
+        // $message = [
+        //     'title.max' => 'Todo title should not be greater than 255 max'
+        // ];
+
+        // $validator = Validator::make($request->all(), $rules, $message);
         
+
+        // if($validator->fails()) {
+        //     return redirect()->back()->withErrors($validator)->withInput();
+        // }
+
         //error of this type of validation goes into the sessions as name of 'errors'
-        $request->validate([
-            'title' => 'required',
-        ]);
+        // $request->validate([
+        //     'title' => 'required|max:255',
+        // ]);
         
         Todo::create($request->all());
 
